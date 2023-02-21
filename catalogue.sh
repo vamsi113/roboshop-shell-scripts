7dfd1f77f9f55c99a4648a47868fc62faddf43a5
vamsi113 <krishultimate5@gmail.com>
2023-02-21 17:44:47 -0500

    adding moongodb script |variables|print|redirectors|exit status|Sed Editor

--- catalogue.sh
+++ catalogue.sh
@@ -2,45 +2,84 @@ LOG_FILE=/tmp/catalogue
 
 echo "setting up nodejs"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-echo Status = $?
+if [$? -eq 0]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo  "installing nodejs"
 yum install nodejs -y &>>${LOG_FILE}
-echo status =$?
+if [$? -eq 0]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo "adding Roboshop application User "
 useradd roboshop &>>${LOG_FILE}
-echo status =$?
+if [$? -eq 0]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo "download catalogue application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-echo status =$?
+if [$? -eq 0]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 cd /home/roboshop
 
 echo "Extracting catalogue application code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
-echo status =$?
+if [$? -eq 0]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 
 echo "installing nodejs dependancies"
 npm install &>>${LOG_FILE}
-echo status =$?
+if [$? -eq 0]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo "setup catalogue service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-echo status =$?
-
+if [$? -eq 0]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 echo "daemon reload"
 systemctl daemon-reload &>>${LOG_FILE}
-echo status =$?
+if [$? -eq 0]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo "application starting"
 systemctl start catalogue &>>${LOG_FILE}
-echo status =$?
+if [$? -eq 0]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo "application starting at boot level as well"
 systemctl enable catalogue &>>${LOG_FILE}
-echo status =$?
+if [$? -eq 0]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
--- frontend.sh
+++ frontend.sh
@@ -28,7 +28,3 @@ systemctl restart nginx &>>$LOG_FILE
 echo status =$?
 
 
-## problems:
-#Ensured the script should run only if it is a root user/sudo previleges.
-#if a ny command is a failure then i need to stop the script there and then.
-#rather than printing 0 and 1 or  something i printed success or failure
