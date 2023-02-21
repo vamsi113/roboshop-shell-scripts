d710b8d211be952d59322c2b1b4fc0cf6c31307e
vamsi113 <krishultimate5@gmail.com>
2023-02-21 17:52:06 -0500

    adding moongodb script |variables|print|redirectors|exit status|Sed Editor|if conditions

--- catalogue.sh
+++ catalogue.sh
@@ -3,33 +3,33 @@ LOG_FILE=/tmp/catalogue
 echo "setting up nodejs"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
 if [$? -eq 0]; then
-  echo Status = Success
+  echo Status = SUCCESS
 else
-  echo Status = Failure
+  echo Status = FAILURE
 fi
 
 echo  "installing nodejs"
 yum install nodejs -y &>>${LOG_FILE}
 if [$? -eq 0]; then
-  echo Status = Success
+  echo Status = SUCCESS
 else
-  echo Status = Failure
+  echo Status = FAILURE
 fi
 
 echo "adding Roboshop application User "
 useradd roboshop &>>${LOG_FILE}
 if [$? -eq 0]; then
-  echo Status = Success
+  echo Status = SUCCESS
 else
-  echo Status = Failure
+  echo Status = FAILURE
 fi
 
 echo "download catalogue application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
 if [$? -eq 0]; then
-  echo Status = Success
+  echo Status = SUCCESS
 else
-  echo Status = Failure
+  echo Status = FAILURE
 fi
 
 cd /home/roboshop
@@ -37,9 +37,9 @@ cd /home/roboshop
 echo "Extracting catalogue application code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
 if [$? -eq 0]; then
-  echo Status = Success
+  echo Status = SUCCESS
 else
-  echo Status = Failure
+  echo Status = FAILURE
 fi
 
 mv catalogue-main catalogue
@@ -48,38 +48,38 @@ cd /home/roboshop/catalogue
 echo "installing nodejs dependancies"
 npm install &>>${LOG_FILE}
 if [$? -eq 0]; then
-  echo Status = Success
+  echo Status = SUCCESS
 else
-  echo Status = Failure
+  echo Status = FAILURE
 fi
 
 echo "setup catalogue service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
 if [$? -eq 0]; then
-  echo Status = Success
+  echo Status = SUCCESS
 else
-  echo Status = Failure
+  echo Status = FAILURE
 fi
 echo "daemon reload"
 systemctl daemon-reload &>>${LOG_FILE}
 if [$? -eq 0]; then
-  echo Status = Success
+  echo Status = SUCCESS
 else
-  echo Status = Failure
+  echo Status = FAILURE
 fi
 
 echo "application starting"
 systemctl start catalogue &>>${LOG_FILE}
 if [$? -eq 0]; then
-  echo Status = Success
+  echo Status = SUCCESS
 else
-  echo Status = Failure
+  echo Status = FAILURE
 fi
 
 echo "application starting at boot level as well"
 systemctl enable catalogue &>>${LOG_FILE}
 if [$? -eq 0]; then
-  echo Status = Success
+  echo Status = SUCCESS
 else
-  echo Status = Failure
+  echo Status = FAILURE
 fi
