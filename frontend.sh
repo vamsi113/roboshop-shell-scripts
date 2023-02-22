9278dc3ab531163fc7a96e7294d622aaf711a640
vamsi113 <krishultimate5@gmail.com>
2023-02-22 14:12:58 -0500

    adding frontend script |variables|print|redirectors|exit status|Sed Editor|if conditions|exit command| exit 1|if con for root user| if con for app user| remove old content|color added |maintained dry code using functions | source command to run script at multiple locations

--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,26 @@
 LOG_FILE=/tmp/frontend
+
+source common.sh
+
 echo installing Nginx
 yum install nginx -y &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo downloading Nginx web content
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 cd /usr/share/nginx/html
 
 
 echo removing old content
 rm -rf *  &>>$LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo extracting web content
 unzip /tmp/frontend.zip &>>$LOG_FILE
 mv frontend-main/static/* . &>>$LOG_FILE
-echo status =$?
+StatusCheck $?
 
 echo moving config file to etc
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
@@ -25,6 +28,6 @@ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 echo starting Nginx Service
 systemctl enable nginx &>>$LOG_FILE
 systemctl restart nginx &>>$LOG_FILE
-echo status =$?
+StatusCheck $?
 
 
