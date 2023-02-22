ea16834f41d9db721293afc431329ed4c74ef938
vamsi113 <krishultimate5@gmail.com>
2023-02-22 14:14:48 -0500

    adding frontend script |variables|print|redirectors|exit status|Sed Editor|if conditions|exit command| exit 1|if con for root user| if con for app user| remove old content|color added |maintained dry code using functions | source command to run script at multiple locations

--- mongodb.sh
+++ mongodb.sh
@@ -4,15 +4,15 @@ source common.sh
 
 echo "setting MongoDB repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
-echo Status =$?
+StatusCheck $?
 
 echo "installing mongodb repo"
 yum install -y mongodb-org &>>$LOG_FILE
-echo Status =$?
+StatusCheck $?
 
 echo "updating listener config"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo Status =$?
+StatusCheck $?
 
 echo "starting mongodb service"
 systemctl enable mongod &>>$LOG_FILE
@@ -25,16 +25,16 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongo
 cd /tmp
 echo "extracting schema files"
 unzip mongodb.zip &>>$LOG_FILE
-echo Status =$?
+StatusCheck $?
 
 
 cd mongodb-main
 
 echo "load Catalogue Service Schema"
 mongo < catalogue.js &>>$LOG_FILE
-echo Status =$?
+StatusCheck $?
 
 echo "Load User Service Schema"
 mongo < users.js &>>$LOG_FILE
-echo Status =$?
+StatusCheck $?
 
