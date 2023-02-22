a966966462a755f90b55ea268c14bc7adb9cb129
vamsi113 <krishultimate5@gmail.com>
2023-02-22 13:44:33 -0500

    adding catalogue script |variables|print|redirectors|exit status|Sed Editor|if conditions|exit command| exit 1|if con for root user| if con for app user| remove old content|color added |maintained dry code using functions |

--- catalogue.sh
+++ catalogue.sh
@@ -6,112 +6,67 @@ if [ $ID -ne 0 ]; then
   exit 1
 fi
 
+## maintaing dry-code using functions
+StatusCheck() {
+  if [ $1 -eq 0 ]; then
+    echo -e Status = "\e[32mSUCCESS\e[0m"
+  else
+    echo -e Status = "\e[32mFAILURE\e[0m"
+    exit 1
+  fi
+}
+
 echo "setting up nodejs"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
+StatusCheck $?
 
-if [ $? -eq 0 ]; then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-else
-  echo -e Status = "\e[32mFAILURE\e[0m"
-  exit 1
-fi
 
 echo  "installing nodejs"
 yum install nodejs -y &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-else
-  echo -e Status = "\e[32mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 id roboshop &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
   echo "adding Roboshop application User "
   useradd roboshop &>>${LOG_FILE}
-  if [ $? -eq 0 ]; then
-    echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-    echo -e Status = "\e[32mFAILURE\e[0m"
-    exit 1
-fi
+  StatusCheck $?
 fi
 
 
 
 echo "download catalogue application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-else
-  echo -e Status = "\e[32mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 cd /home/roboshop
 
 echo "remove old content"
 rm -rf catalogue &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-else
-  echo -e Status = "\e[32mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 echo "Extracting catalogue application code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-else
-  echo -e Status = "\e[32mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 
 echo "installing nodejs dependancies"
 npm install &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-else
-  echo -e Status = "\e[32mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 echo "setup catalogue service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-else
-  echo -e Status = "\e[32mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 echo "daemon reload"
 systemctl daemon-reload &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-else
-  echo -e Status = "\e[32mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 echo "application starting"
 systemctl restart catalogue &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-else
-  echo -e Status = "\e[32mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
 
 echo "application starting at boot level as well"
 systemctl enable catalogue &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo -e Status = "\e[32mSUCCESS\e[0m"
-else
-  echo -e Status = "\e[32mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck $?
