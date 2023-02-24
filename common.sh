7a2953c28c5d2c0243c594427696c4d00ea6b7df
vamsi113 <krishultimate5@gmail.com>
2023-02-24 11:04:29 -0500

    updating roboshop-shell-scripts

--- common.sh
+++ common.sh
@@ -13,3 +13,63 @@ StatusCheck() {
     exit 1
   fi
 }
+
+## NodeJS function Dry Code
+NODEJS(){
+  echo "Setup NodeJS repo"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
+  StatusCheck $?
+
+  echo  "installing nodejs"
+  yum install nodejs -y &>>${LOG_FILE}
+  StatusCheck $?
+
+  id roboshop &>>${LOG_FILE}
+  if [ $? -ne 0 ]; then
+    echo "adding Roboshop application User "
+    useradd roboshop &>>${LOG_FILE}
+    StatusCheck $?
+  fi
+
+
+  echo "download ${COMPONENT} application code"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
+  StatusCheck $?
+
+  cd /home/roboshop
+
+  echo "remove old content"
+  rm -rf ${COMPONENT} &>>${LOG_FILE}
+  StatusCheck $?
+
+  echo "Extracting ${COMPONENT} application code"
+  unzip /tmp/${COMPONENT}.zip &>>${LOG_FILE}
+  StatusCheck $?
+
+  mv ${COMPONENT}-main ${COMPONENT}
+  cd /home/roboshop/${COMPONENT}
+
+  echo "installing nodejs dependancies"
+  npm install &>>${LOG_FILE}
+  StatusCheck $?
+
+  echo "Update SystemD Service File"
+  sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+  StatusCheck $?
+
+  echo "setup user service"
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
+  StatusCheck $?
+
+  echo "daemon reload"
+  systemctl daemon-reload &>>${LOG_FILE}
+  StatusCheck $?
+
+  echo "user application starting"
+  systemctl restart ${COMPONENT} &>>${LOG_FILE}
+  StatusCheck $?
+
+  echo "user application starting at boot level as well"
+  systemctl enable ${COMPONENT} &>>${LOG_FILE}
+  StatusCheck $?
+}
--- user.sh
+++ user.sh
@@ -1,62 +1,9 @@
 LOG_FILE=/tmp/user
 
 source common.sh
+COMPONENT=user
+
+NODEJS
 
 
-echo "Setup NodeJS repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-StatusCheck $?
-
-echo  "installing nodejs"
-yum install nodejs -y &>>${LOG_FILE}
-StatusCheck $?
-
-id roboshop &>>${LOG_FILE}
-if [ $? -ne 0 ]; then
-  echo "adding Roboshop application User "
-  useradd roboshop &>>${LOG_FILE}
-  StatusCheck $?
-fi
-
-
-echo "download user application code"
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>${LOG_FILE}
-StatusCheck $?
-
-cd /home/roboshop
-
-echo "remove old content"
-rm -rf user &>>${LOG_FILE}
-StatusCheck $?
-
-echo "Extracting user application code"
-unzip /tmp/user.zip &>>${LOG_FILE}
-StatusCheck $?
-
-mv user-main user
-cd /home/roboshop/user
-
-echo "installing nodejs dependancies"
-npm install &>>${LOG_FILE}
-StatusCheck $?
-
-echo "Update SystemD Service File"
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/catalogue/user.service
-StatusCheck $?
-
-echo "setup user service"
-mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service &>>${LOG_FILE}
-StatusCheck $?
-
-echo "daemon reload"
-systemctl daemon-reload &>>${LOG_FILE}
-StatusCheck $?
-
-echo "user application starting"
-systemctl restart user &>>${LOG_FILE}
-StatusCheck $?
-
-echo "user application starting at boot level as well"
-systemctl enable user &>>${LOG_FILE}
-StatusCheck $?
 
