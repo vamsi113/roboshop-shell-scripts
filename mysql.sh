52f0a81343533a57ea63c163308bcc8884b1bd1d
vamsi113 <krishultimate5@gmail.com>
2023-02-24 12:09:17 -0500

    updating roboshop-shell-scripts

---
+++ mysql.sh
@@ -0,0 +1,31 @@
+LOG_FILE=/tmp/mysql
+source common.sh
+
+echo "Setting UP MYSQL Repo File"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
+StatusCheck $?
+
+
+echo "Disable MYSQL Default Module to Enable 5.7 MYSQL"
+dnf module disable mysql &>>$LOG_FILE
+StatusCheck $?
+
+
+echo "Install MYSQL"
+yum install mysql-community-server -y &>>$LOG_FILE
+StatusCheck $?
+
+echo Start MYSQL Service""
+systemctl enable mysqld &>>$LOG_FILE
+systemctl restart mysqld &>>$LOG_FILE
+StatusCheck $?
+
+grep temp /var/log/mysqld.log
+# mysql -uroot -pRoboShop@1
+> uninstall plugin validate_password;
+# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+# cd /tmp
+# unzip mysql.zip
+# cd mysql-main
+# mysql -u root -pRoboShop@1 <shipping.sql
