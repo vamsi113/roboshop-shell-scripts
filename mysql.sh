53bcfba66c117b2a18b3976389b31dcd715c4752
vamsi113 <krishultimate5@gmail.com>
2023-02-24 13:06:20 -0500

    updating roboshop-shell-scripts

--- mysql.sh
+++ mysql.sh
@@ -25,7 +25,7 @@ DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $
 StatusCheck $?
 
 echo "Changing mysql Root Password"
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('mypass'); FLUSH PRIVILEGES;" > /tmp/root-pass.sql
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}'); FLUSH PRIVILEGES;" > /tmp/root-pass.sql
 
 
 # mysql -uroot -pRoboShop@1
