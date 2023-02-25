01e92153844200953dbbcc26b137712286b4882e
vamsi113 <krishultimate5@gmail.com>
2023-02-24 21:56:11 -0500

    updating roboshop-shell-scripts

--- common.sh
+++ common.sh
@@ -45,11 +45,6 @@ SYSTEMD_SETUP(){
     sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/'/home/roboshop/${COMPONENT}/systemd.service
     StatusCheck $?
 
-    Environment=CART_HOST=CARTHOST
-    Environment=USER_HOST=USERHOST
-    Environment=AMQP_HOST=AMQPHOST
-
-
   echo "setup ${Component} Service"
     mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
     StatusCheck $?
@@ -120,7 +115,7 @@ PYTHON(){
   echo "Update Payment Configuration File"
   APP_UID=$(id -u roboshop)
   APP_GID=$(id -g roboshop)
-  sed -i -e "s/uid/${APP_UID}/" -e "s/gid/${APP_GID}/" /home/roboshop/payment/payment.ini &>>${LOG_FILE}
+  sed -i -e "c/uid/${APP_UID}/" -e "c/gid/${APP_GID}/" /home/roboshop/payment/payment.ini &>>${LOG_FILE}
   StatusCheck $?
 
   SYSTEMD_SETUP
