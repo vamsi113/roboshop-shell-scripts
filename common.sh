c48423d8d810b3cf170a74fb37c8f711ad86d0a9
vamsi113 <krishultimate5@gmail.com>
2023-02-25 01:04:03 -0500

    updating roboshop-shell-scripts

--- common.sh
+++ common.sh
@@ -42,7 +42,7 @@ APP_PRESETUP(){
 }
 SYSTEMD_SETUP(){
   echo "Update SystemD Service File"
-    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal' -e 's/AMQPHOST/rabbitmq.roboshop.internal'/home/roboshop/${COMPONENT}/systemd.service
     StatusCheck $?
 
   echo "setup ${Component} Service"
