d2942a2e8b728a965f95f1fecc7223f641829f93
vamsi113 <krishultimate5@gmail.com>
2023-03-18 13:20:19 -0400

    updating roboshop-shell-scripts

--- frontend.sh
+++ frontend.sh
@@ -26,7 +26,7 @@ StatusCheck $?
 echo "moving config file to etc"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 
-echo "Updating front config file"
+echo "Updating frontend config file"
 sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 StatusCheck $?
 
